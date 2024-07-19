200d6a5b5fd89f16766fad38f12777c4ad9277a3
dan-kagi <65138417+dan-kagi@users.noreply.github.com>
2024-07-19 10:50:45 -0400

    add hover pseudoclass

--- index.js
+++ index.js
@@ -302,20 +302,6 @@ function createCard(question) {
     event.preventDefault();
   });
 
-  checkBalButton.addEventListener('mouseover', () => {
-    const card = document.getElementById(`${question.id}`);
-    const but = card.querySelector('.balanceamento button');
-    but.classList.remove('buttonStyle');
-    but.classList.add('buttonStyle2');
-  });
-
-  checkBalButton.addEventListener('mouseleave', () => {
-    const card = document.getElementById(`${question.id}`);
-    const but = card.querySelector('.balanceamento button');
-    but.classList.remove('buttonStyle2');
-    but.classList.add('buttonStyle');
-  });
-
   const divBalButton = document.createElement('div');
   divBalButton.appendChild(checkBalButton);
   balForm.appendChild(divBalButton);
@@ -372,19 +358,7 @@ function createCard(question) {
     }
   });
 
-  checkStoichButton.addEventListener('mouseover', () => {
-    const card = document.getElementById(`${question.id}`);
-    const but = card.querySelector('.stoichiometry button');
-    but.classList.remove('buttonStyle');
-    but.classList.add('buttonStyle2');
-  });
-
-  checkStoichButton.addEventListener('mouseleave', () => {
-    const card = document.getElementById(`${question.id}`);
-    const but = card.querySelector('.stoichiometry button');
-    but.classList.remove('buttonStyle2');
-    but.classList.add('buttonStyle');
-  });
+  //
 
   buttonDiv.appendChild(checkStoichButton);
   stoichForm.appendChild(buttonDiv);
